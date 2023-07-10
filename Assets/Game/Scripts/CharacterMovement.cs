5e5a60e7a2ae6040f6a432ae0450088a44d8db42
Gabriel Dechichi <gabrieldechichi@gmail.com>
2023-07-10 15:05:43 -0700

    2 - Character Movement Basics

--- Assets/Game/Scripts/CharacterMovement.cs
+++ Assets/Game/Scripts/CharacterMovement.cs
@@ -1,8 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
+using KinematicCharacterController;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public struct CharacterMovementInput
 {
+    public Vector2 MoveInput;
+}
 
+[RequireComponent(typeof(KinematicCharacterMotor))]
+public class CharacterMovement : MonoBehaviour, ICharacterController
+{
+    public KinematicCharacterMotor Motor;
+
+    public float MaxSpeed;
+    public float Acceleration;
+    public float RotationSpeed;
+
+    private Vector3 moveInput;
+
+    private void Awake()
+    {
+        Motor.CharacterController = this;
+    }
+
+    public void SetInput(in CharacterMovementInput input)
+    {
+        moveInput = Vector3.zero;
+        if (input.MoveInput != Vector2.zero)
+        {
+            moveInput = new Vector3(input.MoveInput.x, 0, input.MoveInput.y).normalized;
+        }
+    }
+
+    public void UpdateRotation(ref Quaternion currentRotation, float deltaTime)
+    {
+        if (moveInput != Vector3.zero)
+        {
+            var targetRotation = Quaternion.LookRotation(moveInput);
+            currentRotation = Quaternion.Slerp(currentRotation, targetRotation, RotationSpeed * deltaTime);
+        }
+    }
+
+    public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
+    {
+        var targetVelocity = moveInput * MaxSpeed;
+        // currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Acceleration * deltaTime);
+        currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, Acceleration * deltaTime);
+    }
+
+    #region not implemented
+
+    public void BeforeCharacterUpdate(float deltaTime)
+    {
+    }
+
+    public void PostGroundingUpdate(float deltaTime)
+    {
+    }
+
+    public void AfterCharacterUpdate(float deltaTime)
+    {
+    }
+
+    public bool IsColliderValidForCollisions(Collider coll)
+    {
+        return true;
+    }
+
+    public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint,
+        ref HitStabilityReport hitStabilityReport)
+    {
+    }
+
+    public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint,
+        ref HitStabilityReport hitStabilityReport)
+    {
+    }
+
+    public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint,
+        Vector3 atCharacterPosition,
+        Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport)
+    {
+    }
+
+    public void OnDiscreteCollisionDetected(Collider hitCollider)
+    {
+    }
+
+    #endregion
 }
--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CharacterMovement characterMovement;
     private void Update()
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
         
-        Debug.Log($"{h}, {v}");
+        characterMovement.SetInput(new CharacterMovementInput()
+        {
+            MoveInput = new Vector2(h, v)
+        });
     }
 }

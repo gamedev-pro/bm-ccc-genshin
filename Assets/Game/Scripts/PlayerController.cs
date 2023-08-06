5a40cc4c9203c3034449fdc95fb154fc04af696f
Gabriel Dechichi <gabrieldechichi@gmail.com>
2023-08-06 13:09:55 -0700

    6 - Input sysetm

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,25 +1,48 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterMovement characterMovement;
     [SerializeField] private CameraController cameraController;
+
+    [Header("Mobile")]
+    [SerializeField] private bool useMobile;
+    [SerializeField] private GameObject mobileCanvas;
+
+    private InputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new InputActions();
+        inputActions.Enable();
+        RefreshOnScreenControls();
+    }
+
+    private void OnValidate()
+    {
+        RefreshOnScreenControls();
+    }
+
+    private void RefreshOnScreenControls()
+    {
+        mobileCanvas.SetActive(useMobile);
+        Cursor.visible = useMobile;
+    }
+
     private void Update()
     {
-        var h = Input.GetAxisRaw("Horizontal");
-        var v = Input.GetAxisRaw("Vertical");
-        var wantsToJump = Input.GetKeyDown(KeyCode.Space);
+        var moveInput = inputActions.Game.Move.ReadValue<Vector2>();
+        var wantsToJump = inputActions.Game.Jump.WasPressedThisFrame();
         
         characterMovement.SetInput(new CharacterMovementInput()
         {
-            MoveInput = new Vector2(h, v),
+            MoveInput = moveInput,
             LookRotation = cameraController.LookRotation,
             WantsToJump = wantsToJump
         });
 
-        var lookX = -Input.GetAxisRaw("Mouse Y");
-        var lookY = Input.GetAxisRaw("Mouse X");
-        cameraController.IncrementLookRotation(new Vector2(lookX, lookY));
-
+        var look = inputActions.Game.Look.ReadValue<Vector2>();
+        cameraController.IncrementLookRotation(new Vector2(look.y, look.x));
     }
 }
